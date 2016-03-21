bf8e0d73f1b2a83e9f407b86a03f08b0482a79f0
hideokamoto <hide.okamoto@digitalcube.jp>
2016-03-21 22:45:50 +0900

    init build react

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var webserver = require('gulp-webserver');
+
+gulp.task('browserify', function() {
+  browserify('./modules/app.jsx', { debug: true })
+    .transform(babelify)
+    .bundle()
+    .on("error", function (err) { console.log("Error : " + err.message); })
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./modules/*.jsx', ['browserify'])
+});
+
+gulp.task('webserver', function() {
+  gulp.src('./')
+    .pipe(webserver({
+      host: '127.0.0.1',
+      livereload: true
+    })
+  );
+});
+
+gulp.task('default', ['browserify', 'watch', 'webserver']);
